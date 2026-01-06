91dfe90e5befcadf95cca8c8bf93984a75f4b65f
Volodymyr <baryliukvova@gmail.com>
2026-01-06 13:41:11 +0200

    add contact css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,27 @@
 function Contact() {
   return (
+    <>
     <div className="contact">
       <div className="name">
-        <p>name</p>
+        <p className="name__title">name</p>
         <input type="text" placeholder="Jane Smith"/>
       </div>
       <div className="email">
-        <p>email</p>
+        <p className="email__title">email</p>
         <input type="text" placeholder="jane@gmail.com"/>
       </div>
-      <div className="massage">
-        <p>massage</p>
-        <input type="text" placeholder="Your massage..."/>
+      <div className="message">
+        <p className="message__title">message</p>
+        <textarea type="text" placeholder="Your message..."/>
       </div>
+      <button className="contaсt__btn">Залиште дані, і ми зв'яжимось з вами</button>
     </div>
+    <div className="social">
+      <p className="social__title">Соціальні мережі</p>
+      <p className="social__subtitle">Знайдіть нас у соціальних мережах для новин.</p>
+      <p className="social__link">Instagram: <a href="https://www.instagram.com/barylux.ua/">@Barylux</a></p>
+    </div>
+    </>
   );
 }
 export default Contact;
